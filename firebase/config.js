1db47373784fdfb95316f5abb8f6b1aa5162d14a
lexfernandes <sr.lexfernandes@gmail.com>
2025-04-09 20:01:03 -0300

    configuracao firebase

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXgPsB5hNyGDnoxultxpUuszFdrwRnXKk",
+  authDomain: "miniblog-dc881.firebaseapp.com",
+  projectId: "miniblog-dc881",
+  storageBucket: "miniblog-dc881.firebasestorage.app",
+  messagingSenderId: "382116969259",
+  appId: "1:382116969259:web:d16b437a328ce533bca36e",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
