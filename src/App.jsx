4aa6b6c4804012c752681e52c066b6b23b4dc58b
lexfernandes <sr.lexfernandes@gmail.com>
2025-04-12 17:14:45 -0300

    add react router, create pages App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
-
 import "./App.css";
+
+// pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+import {
+  BrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>Mini blog</h1>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
